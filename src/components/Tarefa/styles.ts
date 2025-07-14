5aeff022cf9a74bc1cd7cd6798c649c9c2e37e4f
Vini <vini@teste>
2025-07-14 15:03:59 -0300

    projeto finalizado

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Botao } from '../../styles'
 import variaveis from '../../styles/variaveis'
 
 import * as enums from '../../utils/enums/Tarefa'
@@ -28,12 +29,18 @@ export const Card = styled.div`
   padding: 16px;
   margin-bottom: 32px;
   border-radius: 16px;
+
+  label {
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
+  }
 `
 
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: bold;
-  margin-bottom: 16px;
+  margin-left: 8px;
 `
 
 export const Tag = styled.span<TagProps>`
@@ -64,22 +71,6 @@ export const Descricao = styled.textarea`
 export const BarraAcoes = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.1);
   padding-top: 16px;
-`
-
-export const Botao = styled.button`
-  font-size: 12px;
-  font-weight: bold;
-  color: #fff;
-  padding: 8px 12px;
-  border: none;
-  cursor: pointer;
-  background-color: #2F3640;
-  border-radius: 8px;
-  margin-right: 8px;
-`
-
-export const BotaoSalvar = styled(Botao)`
-  background-color: ${variaveis.verde};
 `
 
 export const BotaoCancelarRemover = styled(Botao)`
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,21 +1,42 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 import FiltroCard from "../../components/FiltroCard"
+import { RootReducer } from "../../store"
+import { alteraTermo } from "../../store/reducers/filtro"
 
 import * as S from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+import { Botao, Campo } from '../../styles'
 
-const BarraLateral = () => (
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({ mostrarFiltros }: Props) => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+  return (
   <S.Aside>
     <div>
-      <S.Campo type="text" placeholder="Buscar"/>
-    <S.Filtros>
-        <FiltroCard legenda="pendentes" contador={1} />
-        <FiltroCard legenda="concluídas" contador={2} />
-        <FiltroCard legenda="urgentes" contador={3} />
-        <FiltroCard legenda="importantes" contador={4} />
-        <FiltroCard legenda="normal" contador={5} />
-        <FiltroCard  legenda="todas" contador={10} ativo/>
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+      {mostrarFiltros ? (
+      <>
+      <Campo type="text" placeholder="Buscar" value={termo} onChange={(evento) => dispatch(alteraTermo(evento.target.value))}/>
+      <S.Filtros>
+          <FiltroCard valor={enums.Status.PENDENTE} criterio='status' legenda="pendentes" />
+          <FiltroCard valor={enums.Status.CONCLUIDA} criterio='status' legenda="concluídas" />
+          <FiltroCard valor={enums.Prioridade.URGENTE} criterio='prioridade' legenda="urgentes" />
+          <FiltroCard valor={enums.Prioridade.IMPORTANTE} criterio='prioridade' legenda="importantes" />
+          <FiltroCard valor={enums.Prioridade.NORMAL} criterio='prioridade' legenda="normal" />
+          <FiltroCard criterio='todas' legenda="todas" />
+        </S.Filtros>
+        </>
+      ) : (
+        <Botao onClick={() => navigate("/")}>Voltar a lista de tarefas</Botao>
+      )}
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
